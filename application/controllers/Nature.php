c99e09b407dd0caccc1b7bbeb0eaf9472ab95307
Mae Yee <mae.cst@hotmail.com>
2015-01-29 00:11:44 -0800

    created trivial controllers for the views Signed-off-by:Mae Yee <mae.cst@hotmail.com>

--- application/controllers/Nature.php
+++ application/controllers/Nature.php
@@ -37,30 +37,18 @@
  */
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Nature extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see http://codeigniter.com/user_guide/general/urls.html
 	 */
 	public function index()
 	{
-		$this->load->view('welcome');
-//        $this->data['pagebody'] = 'welcome';
+		$this->load->view('nature');
+//        $this->data['pagebody'] = 'nature';
 //        $this->render();
 	}
 }
 
-/* End of file welcome.php */
-/* Location: ./application/controllers/Welcome.php */
+/* End of file nature.php */
+/* Location: ./application/controllers/Nature.php */
