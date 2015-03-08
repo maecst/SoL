3a55a616fda216f985006688cd0f855922e3e213
Stephanie Lachapelle <steph-lachapelle@hotmail.com>
2015-03-08 14:41:44 -0700

    Edited signin view, removed content covered in template Signed-off-by:Stephanie Lachapelle <steph-lachapelle@hotmail.com>

--- application/views/signin.php
+++ application/views/signin.php
@@ -1,57 +1,3 @@
-<?php
-
-	include 'db/functions.php';
-	require_once('db/config.php');
-	session_start();
-
-	// Connect to server and select database.
-	mysql_connect(DB_HOST, DB_USER, DB_PASSWORD)or die("cannot connect, error: ".mysql_error());
-	mysql_select_db(DB_DATABASE)or die("cannot select DB, error: ".mysql_error());
-	$tbl_name="members"; // Table name
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="utf-8">
-<title>Members Sign In | Sliver of Light Photography</title>
-<link rel="stylesheet" href="style/base.css" media="screen">
-<link rel="stylesheet" href="style/baseprint.css" media="print">
-<link rel="icon" type="image/png" href="images/favicon.png">
-
-<script type="text/javascript" language="Javascript" src="js/formValidation.js"></script>
-</head>
-
-<body>
-
-<div id="wrapper">
-
-<!-- logo -->
-<a href="index">
-	<div id="logo">
-		<p>Logo</p>
-	</div>		
-</a>
-
-<!-- header -->
-<div id="header">
-	<svg id="title" xmlns="http://www.w3.org/2000/svg" pointer-events="none"></svg>
-	<a href="index">
-		<div id="titleLink">
-			Home
-		</div>
-	</a>
-</div>
-
-<?php
-	include_once "db/welcome_header.php";
-?>
-
-<!-- nav -->
-<?php
-	include_once "navbar.php";
-?>
-
 <!-- content -->
 <div id="content" class="signInBackground">
 
@@ -135,13 +81,3 @@
 		</div><!--registerContainer-->
 	</div><!-- regOverflow -->
 </div><!-- content -->
-
-<!-- footer -->
-<div id="footer">
-	<p>Copyright &copy; 2014 Sliver of Light Photography</p>
-</div>
-
-</div><!-- wrapper -->
-
-</body>
-</html>
